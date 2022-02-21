d119e6dd4c310625d3d4ee719bac621ac2d1a450
Dev@HathTech <dm.dev@hathtech.com>
2022-02-21 09:43:44 +0545

    queue implemented

---
+++ queue/queue_basics.py
@@ -0,0 +1,138 @@
+
+# ? implementing queue using list
+
+# price_list_queue = []
+
+
+# price_list_queue.insert(0, 131.5)
+# price_list_queue.insert(1, 132.8)
+# price_list_queue.insert(2, 133.4)
+# price_list_queue.insert(3, 134.7)
+
+# print(price_list_queue)
+
+# price_list_queue.pop()
+
+# print(price_list_queue)
+
+
+# ? implementing queue using deque
+# from collections import deque
+
+
+# q = deque()
+
+# q.appendleft(5)
+# q.appendleft(2)
+# q.appendleft(4)
+# q.appendleft(6)
+
+# print(q)
+# q.pop()
+# q.pop()
+# q.pop()
+# q.pop()
+# print(q)
+
+
+# ? implementing queue using deque
+
+from collections import deque
+
+
+class Queue:
+
+    def __init__(self):
+        self.buffer = deque()
+
+    def enque(self, val):
+        self.buffer.appendleft(val)
+
+    def dequeue(self):
+        return self.buffer.pop()
+
+    def is_empty(self):
+        return len(self.buffer) == 0
+
+    def size(self):
+        return len(self.buffer)
+
+
+pq = Queue()
+
+pq.enque({
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496"
+        }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+        "name": "Romaguera-Crona",
+        "catchPhrase": "Multi-layered client-server neural-net",
+        "bs": "harness real-time e-markets"
+    }
+})
+
+pq.enque({
+    "id": 2,
+    "name": "Ervin Howell",
+    "username": "Antonette",
+    "email": "Shanna@melissa.tv",
+    "address": {
+        "street": "Victor Plains",
+        "suite": "Suite 879",
+        "city": "Wisokyburgh",
+        "zipcode": "90566-7771",
+        "geo": {
+            "lat": "-43.9509",
+            "lng": "-34.4618"
+        }
+    },
+    "phone": "010-692-6593 x09125",
+    "website": "anastasia.net",
+    "company": {
+        "name": "Deckow-Crist",
+        "catchPhrase": "Proactive didactic contingency",
+        "bs": "synergize scalable supply-chains"
+    }
+})
+
+pq.enque({
+    "id": 3,
+    "name": "Clementine Bauch",
+    "username": "Samantha",
+    "email": "Nathan@yesenia.net",
+    "address": {
+        "street": "Douglas Extension",
+        "suite": "Suite 847",
+        "city": "McKenziehaven",
+        "zipcode": "59590-4157",
+        "geo": {
+            "lat": "-68.6102",
+            "lng": "-47.0653"
+        }
+    },
+    "phone": "1-463-123-4447",
+    "website": "ramiro.info",
+    "company": {
+        "name": "Romaguera-Jacobson",
+        "catchPhrase": "Face to face bifurcated interface",
+        "bs": "e-enable strategic applications"
+    }
+})
+
+print(pq.buffer)
+pq.dequeue()
+
+print(pq.buffer)
